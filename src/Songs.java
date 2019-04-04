65693d06ac11cfed1455eb056600a073039ad2c4
Atika Tantiya <atika.tantiya@gmail.com>
2019-04-04 07:01:38 +0530

    changes made

--- src/Songs.java
+++ src/Songs.java
@@ -1,15 +1,13 @@
-import java.awt.*;
+import java.awt.EventQueue;
 import java.sql.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+
 import java.awt.event.*;
 
-public class Home extends JFrame {
+public class Songs extends JFrame {
 
-	private JPanel contentPane;
-	private JTextField txtEmail;
-	private JPasswordField txtPassword;
-
+	private JPanel contentPane2;
 	/**
 	 * Launch the application.
 	 */
@@ -17,8 +15,8 @@ public class Home extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Home frame = new Home();
-					frame.setVisible(true);
+					Songs frame2 = new Songs();
+					frame2.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -29,35 +27,26 @@ public class Home extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Home() {
+	public Songs() {
 		getContentPane().setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane2 = new JPanel();
+		contentPane2.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane2);
+		contentPane2.setLayout(null);
 
 		JPanel panel = new JPanel();
-		panel.setBounds(12, 24, 426, 271);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		
-		txtPassword = new JPasswordField();
-		txtPassword.setBounds(154, 105, 117, 19);
-		panel.add(txtPassword);
+		panel.setBounds(0, 0, 438, 261);
+		contentPane2.add(panel);
+		panel.setLayout(null);		
 
 		JLabel lblEmail = new JLabel("Email Id");
 		lblEmail.setHorizontalAlignment(SwingConstants.LEFT);
 		lblEmail.setBounds(154, 12, 158, 25);
-		panel.add(lblEmail);
+		panel.add(lblEmail);		
 
-		txtEmail = new JTextField();
-		txtEmail.setBounds(154, 49, 114, 19);
-		panel.add(txtEmail);
-		txtEmail.setColumns(10);
-
-		JButton btnSearch = new JButton("Login");
+		JButton btnSearch = new JButton("Search");
 
 		btnSearch.setBounds(154, 171, 117, 25);
 		panel.add(btnSearch);
@@ -69,40 +58,43 @@ public class Home extends JFrame {
 		JLabel lblData = new JLabel("");
 		lblData.setBounds(79, 214, 297, 25);
 		panel.add(lblData);
+		
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
-					String email = txtEmail.getText();
-					String password = String.valueOf(txtPassword.getPassword());
 					
+
+					// Get driver class
 					Class.forName("oracle.jdbc.driver.OracleDriver");
-					
-					Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM","atika123");
-					
+
+					// step2 create the connection object
+					Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM",
+							"atika123");
+
+					// create statement
 					Statement stmt = con.createStatement(
 						    ResultSet.TYPE_SCROLL_INSENSITIVE,
 						    ResultSet.CONCUR_READ_ONLY
 						);
 
-					ResultSet rs = stmt.executeQuery("SELECT * FROM users where email = '" + email + "' and password = '" + password + "'");
-					
+					ResultSet rs = stmt.executeQuery(
+							"SELECT * FROM users");
 					if (!rs.next()) {										
 						System.out.println("Incorrect credientials");
 					} else {
-						
+						// reset to first row
 						rs.beforeFirst();
-						while (rs.next()) {							
-							System.out.println(rs.getInt(1) + " " + rs.getString(2));							
+						while (rs.next()) {
+							//System.out.println("Not empty");
+							System.out.println(rs.getInt(1) + " " + rs.getString(2));
+							// lblData.setText(rs.getString("phone_num") + " " + rs.getString ("name"));
 						}
 					}
-					//con.commit();					
-					con.close();
-					Songs frame2 = new Songs();
-					frame2.setVisible(true);
-					dispose();
 
-				} 
-				catch (Exception e) {
+					//con.commit();
+					con.close();
+
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 
